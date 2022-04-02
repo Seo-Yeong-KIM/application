9fbdd91877a4c3bf65ab92bc7ea92890edc3ecf1
SEOYEONG-KIM <SEOYEONG-KIM@DESKTOP-95I0BFF>
2022-04-02 10:46:04 +0900

    commit

---
+++ src/opertors/Exam03.java
@@ -0,0 +1,15 @@
+package opertors;
+
+public class Exam03 {
+
+	public static void main(String[] args) {
+		// 주어진 숫자에서 100이하를 버린 숫자는 얼마인가요 ?
+		int num = 437;
+		
+		System.out.println(num / 100 * 100);
+		System.out.println(num - num % 100);
+
+		
+	}
+
+}
